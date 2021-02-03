c679792b5eec0703b0efc722f00399e7d290e38a
Manoj Vemuri <manoj.vemuri@outlook.com>
2021-02-03 11:00:40 +0000

    structure

--- question 2,3,4/Book.java
+++ question 2,3,4/Book.java
@@ -1,9 +1,9 @@
 package me;
 
 public class Book {
-  // this are the attributes for the book class
+  // declaring attributes
   String bookName;
-  int price;
+  int cost;
   String author;
   String genre;
 
@@ -12,10 +12,10 @@ public class Book {
   }
 
   // parameterised constructor to set values
-  public Book(String bookName, int price, String author, String genre) {
+  public Book(String bookName, int cost, String author, String genre) {
     super();
     this.bookName = bookName;
-    this.price = price;
+    this.cost = cost;
     this.author = author;
     this.genre = genre;
   }
@@ -29,12 +29,12 @@ public class Book {
     this.bookName = bookName;
   }
 
-  public int getPrice() {
-    return price;
+  public int getcost() {
+    return cost;
   }
 
-  public void setPrice(int price) {
-    this.price = price;
+  public void setcost(int cost) {
+    this.cost = cost;
   }
 
   public String getAuthor() {
@@ -58,6 +58,6 @@ public class Book {
 
   @Override
   public String toString() {
-    return "Book [bookName=" + bookName + ", price=" + price + ", author=" + author + ", genre=" + genre + "]";
+    return "Book [bookName=" + bookName + ", cost=" + cost + ", author=" + author + ", genre=" + genre + "]";
   }
 }
